75927b3d80648165009d5f5b53c3c65979e0f59a
Samemaru07 <osame0830@icloud.com>
2025-05-25 05:16:23 +0900

    pushMessage.batのコメントを削除

--- pushmessage.bat
+++ pushmessage.bat
@@ -1,30 +1,12 @@
 @echo off
 
-:: MySQLを起動する
 start "" "C:\xampp\mysql_start.bat"
 timeout /t 10 /nobreak >nul
-@REM for /f : コマンドの実行結果orテキストファイルの内容を1行ずつ処理
-    @REM "tokens=1-2 delims= " : スペース区切りで1番目と2番目のデータを取得
-    @REM %%a : 1つ目のデータの受け皿
-    @REM date /t : "2025/04/17 木"が得られる
-    @REM do : 処理の開始
-    @REM set today=%%b : 2つめのデータ(曜日)をtodayに代入
-    @REM %%aや%%bはfor文内の一時変数で、ループないだけ有効でループ内の値を一時的に格納できる
 
 for /f "tokens=1-2 delims= " %%a in ('date /t') do set today=%%b
 
-@REM %TIME%は環境変数でコマンド実行前に展開される -> echo 13:45:22.34 となる。
 for /f "tokens=2 delims=:" %%a in ('%TIME%') do set minute=%%a
 
-@REM if文
-    @REM /I : 大文字小文字を区別しない
-    @REM 月～金ならラベルweekday系にジャンプ
-    @REM それ以外(休日)ならラベルholidayにジャンプ
-    @REM 変数名に%%をつけることで変数の中身を参照できる
-
-@REM php -f ファイルパス : PHPファイルを実行し、
-    @REM echo 実行結果の確認用メッセージ
-    @REM exit /b : 呼び出し元に戻らず、このスクリプトを終了
 if /I "%today%" == "月" (
     if /I "%minute%" == "30" (
         goto weekday_backhome
